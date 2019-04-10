2cb9772de4ce7886bfee5a251dc5a93e9826b8f7
chen <aa34913@gmail.com>
2019-04-10 16:08:15 +0800

    commit first

--- bin/index.js
+++ bin/index.js
@@ -10,11 +10,11 @@ const process = async () => {
     console.log(status)
     const res = status.match(/^On branch (.+)\s/);
     console.log('=======CURRENT BRANCH======' + '\n' + res[1]);
+	// keyboard input
+	const commit = await execSync(`git commit -am '${commit_info || "-"}'`, { encoding: 'utf-8' })
+	console.log(commit)
     const _pull = await execSync(`git pull origin ${res[1]}`, { encoding: 'utf-8' })
     console.log(_pull)
-    // 获取输入
-    const commit = await execSync(`git commit -am '${commit_info || "-"}'`, { encoding: 'utf-8' })
-    console.log(commit)
     const _push = await execSync(`git push`, { encoding: 'utf-8' })
     console.log(_push)
 
